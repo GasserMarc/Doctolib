2272d213588e6d3e45e5893b38dbe7336b0bb4ed
victoiredmdt <victoiredemauduit@gmail.com>
2018-11-21 12:05:16 +0100

    efrgt

--- MVP2/Analyse_fonctions.py
+++ MVP2/Analyse_fonctions.py
@@ -23,7 +23,7 @@ def list_functions(Code_candidat): #renvoie une liste de toutes les fonctions du
             list_of_functions.append(remove_specials(mots[i+1]))
     return list_of_functions #renvoie la liste des fonctions et le nombre de fonctions
 
-def trier(list_of_functions): #autre possT
+'''def trier(list_of_functions): #autre possT
     Rlist_of_functions = []
     for function in list_of_functions:
         Rfunction = ''
@@ -34,5 +34,28 @@ def trier(list_of_functions): #autre possT
                 break
         # autre fonction possible : Rfunction = Rfunction.replace("\n",'')
         Rlist_of_functions += [Rfunction]
-    return Rlist_of_functions
+    return Rlist_of_functions'''
+
+def list_tests(tests_candidat):
+    with open (tests_candidat, "r" ) as tests:
+        lecture_tests = tests.readlines()
+        list_of_tests = []
+        for line in lecture_tests:
+            words = line.split()
+            if len(line) < 2: #il y a nécessairement plus de 1 mot sur une ligne définissant un test.
+                pass
+            elif words[0] == 'test':
+                pos1 = line.find('"')
+                borne = pos1+1 # on démarre la nouvelle recherche à partir de l'élément suivant '"'
+                newline = line[borne:]
+                pos2 = newline[pos1:].find('"')
+                list_of_tests.append(line[borne:pos2])
+    return list_of_tests
+
+'''print(list_tests("EventCandidatATest.rb"))
+line = "il a dit : \'non\'"
+print(line)
+pos1 = line.find
+print(line.find('\''))'''
+
 
