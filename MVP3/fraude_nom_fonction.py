1f4ce558666a4b02420d5a9972553c5a70239dbb
fannie4 <fannie4@live.fr>
2018-11-22 11:46:23 +0100

    essaai

--- MVP3/fraude_nom_fonction.py
+++ MVP3/fraude_nom_fonction.py
@@ -1,4 +1,4 @@
-from MVP2.Analyse_fonctions import *
+
 import os
 
 
@@ -18,7 +18,6 @@ def fonction_de_meme_nom(code_candidat):
         #pour chaque fonction du candidat on va ouvrir les anciens codes et verifier le nom
         for fichier in liste_fichier:
             list_functions_comparaison= list_functions(str("../Exemples_codes/" + fichier))
-
             if nom_Fonction in list_functions_comparaison:
                 compteur += 1 #on ajoute 1 si on trouve la fonction dans la base de donnée
         liste_bilan.append(nom_Fonction+ " apparait " + str(compteur)+ " fois dans la base de données")
@@ -34,20 +33,19 @@ def comparaison_code(code_candidat):
         liste_ligne=candidat.readlines()
         occurence=liste_ligne.count("end")
         print(liste_ligne)
-        print(occurence)
         for k in range (occurence):
             liste_ligne.remove("end")#on va pas prendre en compte les end
         for ligne in liste_ligne:
             #Pour chaque ligne du code candidat on va le tester avec les lignes des autres codes de la base de donnée
             # for fichier in liste_fichier:
-                with open("C:\Marc\Coding_weeks\Doctolib\MVP1\EventCandidatA.rb", "r") as code_comparaison:
+                    with open("EventCandidatA.rb", "r") as code_comparaison:
                     #pour chaque code, on crée une liste contenant ses lignes et on va tester si les lignes sont identiques
-                    liste_ligne_comparaison=code_comparaison.readlines()
-                    if ligne in liste_ligne_comparaison:
-                        lignes_identiques += 1
-                        break
+                        liste_ligne_comparaison = code_comparaison.readlines()
+                        if ligne in liste_ligne_comparaison:
+                            lignes_identiques += 1
+                            break
     return(lignes_identiques/len(liste_ligne)) #si renvoie
 
-print(comparaison_code("C:\Marc\Coding_weeks\Doctolib\MVP1\EventCandidatA.rb"))
+print(comparaison_code("EventCandidatA.rb"))
 
 
