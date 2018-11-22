c658e61e91ebd77c5995cdb6e88a34be93092f3e
PiofretB <baptiste.piofret@student-cs.fr>
2018-11-22 18:02:15 +0100

    Premier graphe spider

--- MVP4/analyse_finale.py
+++ MVP4/analyse_finale.py
@@ -4,9 +4,6 @@ from MVP2.tout_MVP2 import *
 from MVP3.fraude_nom_fonction import  *
 import pandas as pd
 
-from Exemples_codes import *
-import numpy as np
-import matplotlib as plt
 
 def analyse_code_candidat (code_candidat):
     analyse ={}
@@ -20,16 +17,16 @@ def analyse_code_candidat (code_candidat):
     #analyse["pourcentage_functions_majuscules"]=majuscule_fonction(code_candidat)
     transformed=transformation_fichier(code_candidat)
     clean=suppr_blank_and_end(suppr_space(transformed))#on enleve les espaces les lignes vides et les lignes end
-    analyse["Duplication_sur_texte_non_nettoye"]=coeff_dice(transformed,0.1)
-    analyse["Duplication_sur_texte_nettoye"]=coeff_dice(clean,0.3)
+    analyse["duplication_sur_texte_non_nettoye"]=coeff_dice(transformed,0.1)
+    analyse["duplication_sur_texte_nettoye"]=coeff_dice(clean,0.3)
     analyse["densite_de_commentaires"]=ratio_commentaires(code_candidat)
     analyse["densite_d_espace"]=ratio_spaces(code_candidat)
     analyse["%detextesuspect"]=comparaison_code(code_candidat)
-    resultats=pd.DataFrame.from_dict(analyse, orient='index')
-    return(resultats)
+    #resultats=pd.DataFrame.from_dict(analyse, orient='index')
+    return analyse #resultats
 
 
-
+#print(analyse_code_candidat("../Exemples_codes/EventCandidateC.rb"))
 
 def note_code_candidat(code_candidat):
     analyse = analyse_code_candidat(code_candidat)
@@ -43,23 +40,23 @@ def note_code_candidat(code_candidat):
     else:
         notes_candidat['taille_moyenne_fonction'] = 4
 
-    if analyse['tooLongLines']>=0.2:
+    if analyse['tooLongLines']>=0.5:
         notes_candidat['tooLongLines'] = 1
-    elif 0.12<=analyse['tooLongLines']<0.2:
+    elif 0.25<=analyse['tooLongLines']<0.5:
         notes_candidat['tooLongLines'] = 2
-    elif 0.05<=analyse['tooLongLines']<0.12:
+    elif 0.1<=analyse['tooLongLines']<0.25:
         notes_candidat['tooLongLines'] = 3
     else:
         notes_candidat['tooLongLines'] = 4
 
-    if 0<=analyse["Duplication_sur_texte"]<0.5:
-        notes_candidat["Duplication_sur_texte"] = 4
-    elif 0.5<=analyse["Duplication_sur_texte"]<1:
-        notes_candidat["Duplication_sur_texte"] = 3
-    elif 1<=analyse["Duplication_sur_texte"]<1.5:
-        notes_candidat["Duplication_sur_texte"] = 2
+    if 0<=analyse["duplication_sur_texte_nettoye"]<0.5:
+        notes_candidat["duplication_sur_texte"] = 4
+    elif 0.5<=analyse["duplication_sur_texte_nettoye"]<1:
+        notes_candidat["duplication_sur_texte"] = 3
+    elif 1<=analyse["duplication_sur_texte_nettoye"]<1.5:
+        notes_candidat["duplication_sur_texte"] = 2
     else:
-        notes_candidat["Duplication_sur_texte"] = 1
+        notes_candidat["duplication_sur_texte"] = 1
 
     if analyse["densite_de_commentaires"] in [0,5] or analyse["densite_de_commentaires"]>40:
         notes_candidat["densite_de_commentaires"] = 1
@@ -79,16 +76,22 @@ def note_code_candidat(code_candidat):
     else:
         notes_candidat["pourcentage_variables_mal_nommees"] = 1
 
+    return notes_candidat
 
 
+#print(note_code_candidat("../Exemples_codes/EventCandidateC.rb"))
 
 def dico_graphe(liste_candidats):
-    dico_candidats={}
-    taille_moyenne_fonction, tooLongLines, duplication_sur_texte, densite_de_commentaires, \
-                              pourcentage_variables_mal_nommees = [], [], [], [], []
-    caracteristiques = [taille_moyenne_fonction, tooLongLines, duplication_sur_texte, densite_de_commentaires, \
-                              pourcentage_variables_mal_nommees]
-    for candidat in liste_candidats:
-        notes_candidats = note_code_candidat(candidat)
-        for car in caracteristiques:
-            car.append(notes_candidats[str(car)])
+    dico_candidats={"candidats":["Candidat " + chr(65+i) for i in range(len(liste_candidats))]}
+    nom_caracteristiques = ["taille_moyenne_fonction", "tooLongLines", "duplication_sur_texte",
+                            "densite_de_commentaires","pourcentage_variables_mal_nommees"]
+    notes_caracteristiques = [[] for x in range(len(nom_caracteristiques))]
+    for i in range(len(nom_caracteristiques)):
+        for candidat in liste_candidats:
+            notes_candidats = note_code_candidat(candidat)
+            notes_caracteristiques[i].append(notes_candidats[nom_caracteristiques[i]])
+        dico_candidats[nom_caracteristiques[i]]=notes_caracteristiques[i]
+    return dico_candidats
+
+#print(dico_graphe(["../Exemples_codes/EventCandidatA.rb", "../Exemples_codes/EventCandidateB.rb",
+#                   "../Exemples_codes/EventCandidateC.rb"]))
