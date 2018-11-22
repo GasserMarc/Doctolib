8df81e38b38c1ac889b9d3c0f1b8a03a6e100373
mp3174 <marie.pavageau@student-cs.fr>
2018-11-22 14:49:24 +0100

    flemme

--- MVP4/analyse_finale.py
+++ MVP4/analyse_finale.py
@@ -1,5 +1,6 @@
 from MVP1.tout_MVP1 import *
 from MVP2.tout_MVP2 import *
+from MVP3.fraude_nom_fonction import  *
 import pandas as pd
 
 from Exemples_codes import *
@@ -15,14 +16,18 @@ def analyse_code_candidat (code_candidat):
     analyse["pourcentage_variables_mal_nommées"]=calcul_pourcentage_variables_mal_nommees(code_candidat)
     analyse["pourcentage_variables_majuscules"]=majuscule_variable(code_candidat)
     analyse["pourcentage_fonctions_mal_nommées"]=calcul_pourcentage_fonctions_mal_nommees(code_candidat)
-    #MVP2["pourcentage_functions_majuscules"]=majuscule_fonction(adresse)
+    #analyse["pourcentage_functions_majuscules"]=majuscule_fonction(code_candidat)
     transformed=transformation_fichier(code_candidat)
     clean=suppr_blank_and_end(suppr_space(transformed))#on enleve les espaces les lignes vides et les lignes end
     analyse["Duplication_sur_texte_non_nettoyé"]=coeff_dice(transformed,0.1)
     analyse["Duplication__sur_texte_nettoyé"]=coeff_dice(clean,0.3)
     analyse["densite_de_commentaires"]=ratio_commentaires(code_candidat)
     analyse["densite_d_espace"]=ratio_spaces(code_candidat)
+    analyse["%lignessuspectes"]= comparaison_code(code_candidat)
     resultats=pd.DataFrame.from_dict(analyse, orient='index')
     return(resultats)
 
-print(analyse_code_candidat("/Users/baptiste/PycharmProjects/Doctolib/Exemples_codes/EventCandidatA.rb"))
+
+print(analyse_code_candidat("C:/Users/Marie/PycharmProjects/Doctolib/Exemples_codes/EventCandidatA.rb"))
+print(analyse_code_candidat("C:/Users/Marie/PycharmProjects/Doctolib/Exemples_codes/EventCandidateB.rb"))
+print(analyse_code_candidat("C:/Users/Marie/PycharmProjects/Doctolib/Exemples_codes/EventCandidateC.rb"))
